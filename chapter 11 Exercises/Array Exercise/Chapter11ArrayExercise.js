53055106f33664adfc66aba7aee97251dd2750d5
Ross Parsons <rwp140@gmail.com>
2017-02-02 21:30:57 -0700

    Fixed file name error Chapter11ArrayExercise

--- chapter 11 Exercises/Array Exercise/Chapter11ArrayExercise.js
+++ chapter 11 Exercises/Array Exercise/Chapter11ArrayExercise.js
@@ -2,6 +2,14 @@
 //to run this file locally see relative support files
 
 //below is moddieified from the given exercises by this name
+//Arrays
+
+//Add support for arrays to Egg by adding the following three
+// functions to the top scope: array(...) to construct an 
+//array containing the argument values, length(array) to get
+// an array’s length, and element(array, n) to fetch the nth
+// element from an array.
+
 // Modify these definitions...
 
 topEnv["array"] = function(){
